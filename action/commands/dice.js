bced75555d98dd5da26a719f93ea90153e62d1f6
anuvarov <anuvarov0103@gmail.com>
2021-06-03 01:27:15 +0500

    1 sekund kutib reply qilish qo'shildi

--- action/commands/dice.js
+++ action/commands/dice.js
@@ -1,31 +1,33 @@
 const { bot } = require('../../core/bot');
 
 bot.on('dice', ctx => {
-    if (ctx.message.dice.emoji === "⚽")
-        ctx.telegram.sendDice(ctx.from.id, {
-            emoji: "⚽"
-        });
+    setTimeout(() => {
+        if (ctx.message.dice.emoji === "⚽")
+            ctx.telegram.sendDice(ctx.from.id, {
+                emoji: "⚽"
+            });
 
-    if (ctx.message.dice.emoji === "🏀")
-        ctx.telegram.sendDice(ctx.from.id, {
-            emoji: "🏀"
-        });
+        if (ctx.message.dice.emoji === "🏀")
+            ctx.telegram.sendDice(ctx.from.id, {
+                emoji: "🏀"
+            });
 
-    if (ctx.message.dice.emoji === "🎳")
-        ctx.telegram.sendDice(ctx.from.id, {
-            emoji: "🎳"
-        });
+        if (ctx.message.dice.emoji === "🎳")
+            ctx.telegram.sendDice(ctx.from.id, {
+                emoji: "🎳"
+            });
 
-    if (ctx.message.dice.emoji === "🎰")
-        ctx.telegram.sendDice(ctx.from.id, {
-            emoji: "🎰"
-        });
-    if (ctx.message.dice.emoji === "🎲")
-        ctx.telegram.sendDice(ctx.from.id, {
-            emoji: "🎲"
-        });
-    if (ctx.message.dice.emoji === "🎯")
-        ctx.telegram.sendDice(ctx.from.id, {
-            emoji: "🎯"
-        });
+        if (ctx.message.dice.emoji === "🎰")
+            ctx.telegram.sendDice(ctx.from.id, {
+                emoji: "🎰"
+            });
+        if (ctx.message.dice.emoji === "🎲")
+            ctx.telegram.sendDice(ctx.from.id, {
+                emoji: "🎲"
+            });
+        if (ctx.message.dice.emoji === "🎯")
+            ctx.telegram.sendDice(ctx.from.id, {
+                emoji: "🎯"
+            });
+    }, 1000);
 });
